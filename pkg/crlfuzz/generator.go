18fd1fd02785a23b2671e65461069dc5388b7239
qazbnm456 <qazbnm456@gmail.com>
2024-02-20 17:41:55 +0800

    fix: crlf test logic

--- pkg/crlfuzz/generator.go
+++ pkg/crlfuzz/generator.go
@@ -2,6 +2,8 @@ package crlfuzz
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -15,9 +17,22 @@ func GenerateURL(u string) []string {
 
 	for _, a := range appendList {
 		for _, e := range escapeList {
-			url = append(url, fmt.Sprint(u, a, e, keyHeader, "%3a%20", valHeader))
+			url = append(url, fmt.Sprint(u, a, DecodeUnicode(e), keyHeader, "%3a%20", valHeader))
 		}
 	}
 
 	return url
+}
+
+func DecodeUnicode(s string) string {
+	re := regexp.MustCompile(`%u[0-9A-Fa-f]{4}`)
+	decodedString := re.ReplaceAllStringFunc(s, func(match string) string {
+		unicodeStr := match[2:]
+		unicodeInt, _ := strconv.ParseInt(unicodeStr, 16, 32)
+
+		return string(rune(unicodeInt))
+	})
+	decodedString = strings.ReplaceAll(decodedString, "%u", "\\u")
+
+	return decodedString
 }
